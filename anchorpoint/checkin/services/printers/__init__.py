3186c140dce6e606d4ae325e74a6f16d7e562f24
TheLVSQ <Luke.Levesque@newpointe.org>
2026-05-10 08:23:27 -0400

    feat: add BrotherQLAdapter, ESCPOSAdapter, and PrintService.print_checkins()

--- anchorpoint/checkin/services/printers/__init__.py
+++ anchorpoint/checkin/services/printers/__init__.py
@@ -1,5 +1,6 @@
 from .base import BasePrinterAdapter
-from .escpos_adapter import ESCPOSAdapter
+from .brother_ql_adapter import BrotherQLAdapter
 from .cups_adapter import CUPSAdapter
+from .escpos_adapter import ESCPOSAdapter
 
-__all__ = ["BasePrinterAdapter", "ESCPOSAdapter", "CUPSAdapter"]
+__all__ = ["BasePrinterAdapter", "BrotherQLAdapter", "CUPSAdapter", "ESCPOSAdapter"]
